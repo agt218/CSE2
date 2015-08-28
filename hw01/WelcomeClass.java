202d25852343922145d2a43e955547e3771c743e
agt218 <agt218@lehigh.edu>
2015-08-28 17:34:33 +0000

    Finished version of homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Alec Tilley
+//Homework 01
+//8-28-15
+//CSE2
+//WelcomeClass program
+//
+//Create a public class
+public class WelcomeClass{
+    //Add a Main Method
+    public static void main(String[] args) {
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-A--G--T--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("When I was only eight months old,");
+        System.out.println("I was adopted from St. Petersburg Russia.");
+    }
+}
