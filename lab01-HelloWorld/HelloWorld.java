e14d150ba4bdf6db5adea169780f6b832e01b8fe
agt218 <agt218@lehigh.edu>
2015-08-28 15:38:23 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//Alec Tilley
+//Demo 0
+//Hello World Java Program
+//
+//  define a class
+public class HelloWorld{
+    //add a main method
+    public static void main(String[] args) {
+        //print the statement withg a box around it
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("#                       #");
+        System.out.println("#   Hello World 2015!!  #");
+        System.out.println("#                       #");
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("test");
+        //testprogram/testing
+    }
+}
