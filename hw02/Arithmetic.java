ea81759354849bbf74078c09695e4f20f528db9f
agt218 <agt218@lehigh.edu>
2015-09-09 02:11:57 +0000

    Homework02 is done

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -6,32 +6,38 @@
 public class Arithmetic{
     //Add a Main Method
     public static void main(String[] args) {
-        //Number of items bought
+        //Setting Number of items bought
         int nSocks=3;
         int nGlasses=6;
         int nEnvelopes=1;
-        //Item Cost
+        //Setting Item Cost
         double sockCost$=2.58;
         double glassesCost$=2.29;
         double envelopeCost$=3.25;
         double taxPercent=0.06;
 
-        //Total cost of each item before sales tax
+        //Calculating Total cost of each item before sales tax
         double totalSockCost= nSocks * sockCost$;
         double totalGlassesCost= nGlasses * glassesCost$;
         double totalEnvelopeCost= nEnvelopes * envelopeCost$;
-        //Total cost of order
+        //Calculating Total cost of order
         double totalOrder= totalSockCost + totalEnvelopeCost + totalGlassesCost;
-        //Total amount of sales tax
+        //Total amount of sales tax in nice decimal form
         double totalTax= totalOrder* taxPercent;
-        //Total cost of order with salex ta
+        double Actualtax=  (totalTax * 100);
+        double Finaltax=(int) Actualtax;
+        double TAX= ((double)Finaltax)/100;
+        //Total cost of order with salex tax in nice decimal form
         double totalOrderCost = totalOrder + totalTax;
+        double Actualorder= (totalOrderCost * 100);
+        double Finalorder= (int) Actualorder;
+        double Order= ((double)Finalorder)/100;
         //Printing the results
         System.out.println("The total cost for 3 socks before tax is $" +totalSockCost );
         System.out.println("The total cost for 6 Glasses before tax is $" +totalGlassesCost );
         System.out.println("The total cosr for 1 envelope is $" +totalEnvelopeCost );
         System.out.println("The total cost for the order before tax is $" +totalOrder );
-        System.out.println("The total amount of sales tax is $" +totalTax );
-        System.out.println("The total amount paid for the order including sales tax is $" + totalOrderCost);
+        System.out.println("The total amount of sales tax is $" +TAX );
+        System.out.println("The total amount paid for the order including sales tax is $" + Order);
     }
 }
